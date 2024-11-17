d45bebe50db1c0c43a5e6f25871ab3ea954ce445
khimphm <phamtrikhiem2524@gmail.com>
2024-11-17 15:50:25 +0700

    nhom3

---
+++ src/views/AboutView.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-btn>
+    Button
+  </v-btn>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
